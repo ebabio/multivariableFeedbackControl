591d758c3fd1012be2e85b9864d8183fdb083367
Enrique Babio <enrique.babio@skydweller.aero>
2020-08-06 09:57:00 +0200

    Completed examples on d_loop_shaping.m: Getting the disturbance TF Design of several controllers

--- 2_classical control/d_loop_shaping.m
+++ 2_classical control/d_loop_shaping.m
@@ -3,44 +3,71 @@
 close all
 clc
 
-%% Define System
+% Add Library
+addpath(fullfile(pwd, 'lib'));
+
+%% Define Plant
 
 % 1. Define the siso open loop model and define the disturbance point
 % subparts
-g1 = zpk_time([], 10, 200);
-g2 = zpk_time([], [0.05 0.05], 1);
+g1 = zpk_time([], [0.05 0.05], 2);
+g2 = zpk_time([], 10, 100);
 d = AnalysisPoint('d');
 
 G = g2 * d * g1;
 
 %% Define Controller
 
-% Inverse-based Controller:
+% K0: Inverse-based Controller:
 % give bandwidth, a integrator for the step and invert plant
 wc = 10;
-K = wc * tf(1, 0) / g1 * zpk_time(0.1, 0.01); %g1 is inverted perfectly thanks to the integrator, g2 is approxed this way
+K = wc * tf(1, [1 0]) / g2 * zpk_time(0.1, 0.01, 1/2); %g1 is inverted perfectly thanks to the integrator, g2 is approxed this way
 
-%% Define generalized system
-% take disturbance as an extra input
-S = getSensitivity(T, 'y');
+% K2: A simple controller with integral action to reject the disturbance
+K = 0.5 * tf([1, 2], [1, 0]);
 
+% K3: The simple controller with derivative action
+K = 0.5 * tf([1, 2], [1, 0]) * zpk_time(0.05, 0.005);
 
-%% Analyze
+Kr = 1; % a unity pre-filter controller
+
+% 2DOF Controller
+% K is taken from K3
+Kr = zpk_time(0.5, [0.65, 0.03]);
+
+%% Construct generalized system
+
+% Define Open Loop System
+L = G * K;
 
 % Get sensitivities
 y = AnalysisPoint('y'); 
 T = feedback(y*L,1);
 S = getSensitivity(T, 'y');
 
+% Add a 2DOF controller
+Tr = T * Kr;
 
-%% Analyze Controller
+% Get TF from disturbance d to output y
+Td = getIOTransfer(T, 'd', 'y');
 
-% Feedback loop TF, i.e. Complementary Sensitivity Function
-y = AnalysisPoint('y'); % for use in sensitivity function
-T = feedback(y*L,1);
+% These are different ways of getting the same realization of Td
+minzpk = @(sys) zpk(minreal(sys, 1e-3));
 
-% Error dynamics, i.e. Sensitivity function
-S = getSensitivity(T, 'y');
+minzpk(Td);
+minzpk(g2 / (1 + g2 * g1 * K));
+minzpk(S * g2);
 
-% Plot step response
-step(T)
+%% Analyze
+
+% Step Response Plots
+tfinal = 3;
+figure(1)
+
+% Input Output response
+subplot(2,1,1)
+step(Tr, tfinal)
+
+% Disturbance Rejection response
+subplot(2,1,2)
+step(Td, tfinal)
